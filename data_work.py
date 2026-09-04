b868d362a393385087aa541bc96ad46ffbeb9d5d
Babayoboy <joshiansh70@gmail.com>
2026-09-04 22:29:20 +0530

    Create data_work.py

---
+++ data_work.py
@@ -0,0 +1,13 @@
+from numpy import int64
+import pandas as pd
+from pandas.core.reshape.melt import to_numeric
+
+dataframe = pd.read_csv("GDP.csv")
+
+dataframe = dataframe.rename(columns = {"GDP Nominal (Current USD)": "GDP(N)", "GDP Real (Constant, Inflation Adjusted)": "GDP(R)"})
+print(dataframe)
+
+dataframe[["GDP(N)", "GDP(R)"]] =  dataframe[["GDP(N)", "GDP(R)"]].replace({r"\$":''}, regex=True)
+print(dataframe)
+dataframe_change = to_numeric(dataframe.pct_change())
+print(dataframe_change)
