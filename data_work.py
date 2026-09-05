aa72029b55610ba163349a3f9e3d5e92d1c2c333
Babayoboy <joshiansh70@gmail.com>
2026-09-05 21:22:25 +0530

    done some work still going strong

--- data_work.py
+++ data_work.py
@@ -5,7 +5,9 @@ dataframe = pd.read_csv("GDP.csv")
 dataframe = dataframe.rename(columns = {"GDP Nominal (Current USD)": "GDP(N)", "GDP Real (Constant, Inflation Adjusted)": "GDP(R)"})
 print(dataframe)
 
-dataframe[["GDP(N)", "GDP(R)"]] =  dataframe[["GDP(N)", "GDP(R)"]].replace({r"\$":''}, regex=True)
+dataframe[["GDP(N)", "GDP(R)", "Pop. Change"]] =  dataframe[["GDP(N)", "GDP(R)", "Pop. Change"]].replace({r"\$":'', ',':'', '%':''}, regex=True).astype(float)
+dataframe.sort_values(by=["Year"], ascending=True, inplace=True)
 print(dataframe)
-dataframe_change = to_numeric(dataframe.pct_change())
-print(dataframe_change)
+dataframe_change = dataframe[["GDP(N)", "GDP(R)"]].pct_change()
+dataframe[["GDP(N) change", "GDP(R) change"]] = dataframe_change
+print(dataframe)
